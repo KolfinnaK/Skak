697a7f8ca8b698fd8ada8f43c302fb392d627a2a
sindrihardar <73977856+sindrihardar@users.noreply.github.com>
2023-04-14 15:43:36 +0000

    laga homebutton aftur

--- src/main/java/vidmot/UpphafController.java
+++ src/main/java/vidmot/UpphafController.java
@@ -78,39 +78,37 @@ public class UpphafController {
         fxHljodtakki.setGraphic(image);
     }
 
+
     public void fxHomeButtonHandler(ActionEvent actionEvent) {
-        fxHomeButton.setOnAction(event -> {
-            if (!event.isConsumed()) {
-                event.consume();
+        fxHomeButton.setOnAction(this::fxHomeButtonHandler);
+        if (!actionEvent.isConsumed()) {
+            actionEvent.consume();
 
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Ertu viss?");
-                alert.setHeaderText(null);
-                alert.setContentText("Vilt þú fara til baka á upphafsskjá og hreinsa þema?");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Ertu viss?");
+            alert.setHeaderText(null);
+            alert.setContentText("Vilt þú fara til baka á upphafsskjá og hreinsa þema?");
 
-                ButtonType yesButton = new ButtonType("Já", ButtonBar.ButtonData.OK_DONE);
-                ButtonType noButton = new ButtonType("Nei", ButtonBar.ButtonData.CANCEL_CLOSE);
+            ButtonType yesButton = new ButtonType("Já", ButtonBar.ButtonData.OK_DONE);
+            ButtonType noButton = new ButtonType("Nei", ButtonBar.ButtonData.CANCEL_CLOSE);
 
-                alert.getButtonTypes().setAll(yesButton, noButton);
+            alert.getButtonTypes().setAll(yesButton, noButton);
 
-                Optional<ButtonType> result = alert.showAndWait();
-                if (result.isPresent() && result.get() == yesButton) {
-                    fxHomeButton.getScene().getStylesheets().clear();
-                    try {
-                        root = FXMLLoader.load(getClass().getResource("/vidmot/upphaf-view.fxml"));
-                        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-                        scene = new Scene(root);
-                        stage.setScene(scene);
-                        stage.show();
-                        isBot = 0;
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == yesButton) {
+                fxHomeButton.getScene().getStylesheets().clear();
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/vidmot/upphaf-view.fxml"));
+                    stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                    isBot = 0;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        });
+        }
     }
 
     public void fxClassicHandler(ActionEvent actionEvent) {
@@ -141,6 +139,7 @@ public class UpphafController {
     }
 
     public void initialize() {
+        fxHomeButton.setOnAction(this::fxHomeButtonHandler);
         ImageView homeIcon = new ImageView(new Image(getClass().getResource("images/home_icon.png").toExternalForm()));
         homeIcon.setFitWidth(30);
         homeIcon.setFitHeight(25);
